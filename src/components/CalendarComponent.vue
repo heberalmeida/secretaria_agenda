f842afc307db0c159688b26d1f7fe3cf54c362a2
Heber Almeida <eu@heber.com.br>
2024-07-24 07:49:29 -0400

    ajustes

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="calendar-container">
-        <CalendarView :show-date="showDate" :items="calendarItems"
+        <CalendarView :show-date="showDate" :items="calendarItems" :itemTop="'1.6em'" :itemBorderHeight="'8px'"
             class="theme-default holiday-us-traditional holiday-us-official" :wrap-item-title-on-hover="true"
             :do-emit-item-mouse-events="true" @item-mouseenter="showTooltip" @item-mouseleave="hideTooltip">
             <template #header="headerProps">
@@ -49,7 +49,7 @@ export default {
                 { id: 11, startDate: "2024-08-03", endDate: "2024-08-03", title: "Culto da UMADECAMP", classes: ["event-write"] },
                 { id: 12, startDate: "2024-08-16", endDate: "2024-08-18", title: "Congresso UFADECAMP", classes: ["event-orange"] },
                 { id: 13, startDate: "2024-08-17", endDate: "2024-08-17", title: "Culto da AADECAAMP", classes: ["event-write"] },
-                { id: 14, startDate: "2024-08-19", endDate: "2024-08-20", title: "Setor 1 - Crianças e adolescentes", classes: ["event-purple"] },
+                { id: 14, startDate: "2024-08-19", endDate: "2024-08-20", title: "Setor1 - Crianças e adolescentes", classes: ["event-purple"] },
                 { id: 15, startDate: "2024-08-22", endDate: "2024-08-22", title: "CCPO", classes: ["event-green"] },
                 { id: 66, startDate: "2024-08-29", endDate: "2024-08-29", title: "CCPO", classes: ["event-green"] },
                 { id: 66, startDate: "2024-09-05", endDate: "2024-09-05", title: "CCPO", classes: ["event-green"] },
@@ -59,26 +59,26 @@ export default {
                 { id: 16, startDate: "2024-08-22", endDate: "2024-08-22", title: "Ivanaldo", classes: ["event-blue"] },
                 { id: 17, startDate: "2024-08-26", endDate: "2024-08-26", title: "Sandro", classes: ["event-blue"] },
                 { id: 18, startDate: "2024-08-29", endDate: "2024-08-29", title: "Leonel", classes: ["event-blue"] },
-                { id: 19, startDate: "2024-09-06", endDate: "2024-09-08", title: "Setor 4", classes: ["event-purple"] },
+                { id: 19, startDate: "2024-09-06", endDate: "2024-09-08", title: "Setor4", classes: ["event-purple"] },
                 { id: 20, startDate: "2024-09-07", endDate: "2024-09-07", title: "Consagração da UFADECAMP", classes: ["event-write"] },
                 { id: 21, startDate: "2024-09-07", endDate: "2024-09-07", title: "Culto da UMADECAMP", classes: ["event-write"] },
-                { id: 22, startDate: "2024-09-07", endDate: "2024-09-07", title: "Setor 9 - Kids", classes: ["event-purple"] },
-                { id: 23, startDate: "2024-09-12", endDate: "2024-09-14", title: "Setor 13", classes: ["event-purple"] },
-                { id: 25, startDate: "2024-09-12", endDate: "2024-09-15", title: "Setor 15", classes: ["event-purple"] },
-                { id: 24, startDate: "2024-09-13", endDate: "2024-09-14", title: "Setor 9 - Irmãs", classes: ["event-purple"] },
-                { id: 25, startDate: "2024-09-13", endDate: "2024-09-15", title: "Setor 3 - Jovens e adolescentes", classes: ["event-purple"] },
-                { id: 25, startDate: "2024-09-13", endDate: "2024-09-15", title: "Setor 6", classes: ["event-purple"] },
-                { id: 25, startDate: "2024-09-13", endDate: "2024-09-15", title: "Setor 14 - Jovens e Adolesc", classes: ["event-purple"] },
-                { id: 25, startDate: "2024-09-13", endDate: "2024-09-15", title: "Setor 19", classes: ["event-purple"] },
+                { id: 22, startDate: "2024-09-07", endDate: "2024-09-07", title: "Setor9 - Kids", classes: ["event-purple"] },
+                { id: 23, startDate: "2024-09-12", endDate: "2024-09-14", title: "Setor13", classes: ["event-purple"] },
+                { id: 25, startDate: "2024-09-12", endDate: "2024-09-15", title: "Setor15", classes: ["event-purple"] },
+                { id: 24, startDate: "2024-09-13", endDate: "2024-09-14", title: "Setor9 - Irmãs", classes: ["event-purple"] },
+                { id: 25, startDate: "2024-09-13", endDate: "2024-09-15", title: "Setor3 - Jovens e adolescentes", classes: ["event-purple"] },
+                { id: 25, startDate: "2024-09-13", endDate: "2024-09-15", title: "Setor6", classes: ["event-purple"] },
+                { id: 25, startDate: "2024-09-13", endDate: "2024-09-15", title: "Setor14 - Jovens e Adolesc", classes: ["event-purple"] },
+                { id: 25, startDate: "2024-09-13", endDate: "2024-09-15", title: "Setor19", classes: ["event-purple"] },
                 { id: 26, startDate: "2024-09-20", endDate: "2024-09-22", title: "Congresso SEMADEMS", classes: ["event-orange"] },
                 { id: 27, startDate: "2024-09-21", endDate: "2024-09-21", title: "Culto da AADECAAMP", classes: ["event-write"] },
                 { id: 28, startDate: "2024-09-24", endDate: "2024-09-24", title: "Batismo", classes: ["event-verde"] },
-                { id: 29, startDate: "2024-09-26", endDate: "2024-09-29", title: "Setor 10", classes: ["event-purple"] },
-                { id: 29, startDate: "2024-09-27", endDate: "2024-09-28", title: "Setor 18", classes: ["event-purple"] },
-                { id: 30, startDate: "2024-09-27", endDate: "2024-09-29", title: "Setor 17 - Jovens", classes: ["event-purple"] },
-                { id: 31, startDate: "2024-10-04", endDate: "2024-10-06", title: "Setor 12", classes: ["event-purple"] },
-                { id: 32, startDate: "2024-10-04", endDate: "2024-10-06", title: "Setor 14 - Irmãs", classes: ["event-purple"] },
-                { id: 33, startDate: "2024-10-04", endDate: "2024-10-06", title: "Setor 16 - Irmãs", classes: ["event-purple"] },
+                { id: 29, startDate: "2024-09-26", endDate: "2024-09-29", title: "Setor10", classes: ["event-purple"] },
+                { id: 29, startDate: "2024-09-27", endDate: "2024-09-28", title: "Setor18", classes: ["event-purple"] },
+                { id: 30, startDate: "2024-09-27", endDate: "2024-09-29", title: "Setor17 - Jovens", classes: ["event-purple"] },
+                { id: 31, startDate: "2024-10-04", endDate: "2024-10-06", title: "Setor12", classes: ["event-purple"] },
+                { id: 32, startDate: "2024-10-04", endDate: "2024-10-06", title: "Setor14 - Irmãs", classes: ["event-purple"] },
+                { id: 33, startDate: "2024-10-04", endDate: "2024-10-06", title: "Setor16 - Irmãs", classes: ["event-purple"] },
                 { id: 34, startDate: "2024-10-05", endDate: "2024-10-05", title: "Consagração da UFADECAMP", classes: ["event-write"] },
                 { id: 35, startDate: "2024-10-05", endDate: "2024-10-05", title: "Culto da UMADECAMP", classes: ["event-write"] },
                 { id: 36, startDate: "2024-10-07", endDate: "2024-10-07", title: "Ivaldo", classes: ["event-blue"] },
@@ -87,23 +87,23 @@ export default {
                 { id: 40, startDate: "2024-10-18", endDate: "2024-10-20", title: "Congresso AADECAAMP", classes: ["event-orange"] },
                 { id: 41, startDate: "2024-11-15", endDate: "2024-11-17", title: "AGO COMADEMS", classes: ["event-lightblue"] },
                 { id: 42, startDate: "2024-10-24", endDate: "2024-10-24", title: "Francemildo", classes: ["event-blue"] },
-                { id: 43, startDate: "2024-10-25", endDate: "2024-10-27", title: "Setor 1 - Irmãs e Jovens", classes: ["event-purple"] },
-                { id: 44, startDate: "2024-10-25", endDate: "2024-10-27", title: "Setor 2", classes: ["event-purple"] },
-                { id: 45, startDate: "2024-10-25", endDate: "2024-10-27", title: "Setor 9 - Jovens e Adolescentes", classes: ["event-purple"] },
+                { id: 43, startDate: "2024-10-25", endDate: "2024-10-27", title: "Setor1 - Irmãs e Jovens", classes: ["event-purple"] },
+                { id: 44, startDate: "2024-10-25", endDate: "2024-10-27", title: "Setor2", classes: ["event-purple"] },
+                { id: 45, startDate: "2024-10-25", endDate: "2024-10-27", title: "Setor9 - Jovens e Adolescentes", classes: ["event-purple"] },
                 { id: 47, startDate: "2024-11-02", endDate: "2024-11-02", title: "Consagração da UFADECAMP", classes: ["event-orange"] },
                 { id: 48, startDate: "2024-11-02", endDate: "2024-11-02", title: "Culto da UMADECAMP", classes: ["event-write"] },
-                { id: 49, startDate: "2024-11-07", endDate: "2024-11-10", title: "Setor 16 - Unificado", classes: ["event-purple"] },
-                { id: 50, startDate: "2024-11-07", endDate: "2024-11-11", title: "Setor 5", classes: ["event-purple"] },
-                { id: 51, startDate: "2024-11-08", endDate: "2024-11-10", title: "Setor 3 - Irmãs", classes: ["event-purple"] },
-                { id: 53, startDate: "2024-11-09", endDate: "2024-11-10", title: "Setor 20", classes: ["event-purple"] },
+                { id: 49, startDate: "2024-11-07", endDate: "2024-11-10", title: "Setor16 - Unificado", classes: ["event-purple"] },
+                { id: 50, startDate: "2024-11-07", endDate: "2024-11-11", title: "Setor5", classes: ["event-purple"] },
+                { id: 51, startDate: "2024-11-08", endDate: "2024-11-10", title: "Setor3 - Irmãs", classes: ["event-purple"] },
+                { id: 53, startDate: "2024-11-09", endDate: "2024-11-10", title: "Setor20", classes: ["event-purple"] },
                 { id: 52, startDate: "2024-11-14", endDate: "2024-11-14", title: "Willian Pereira", classes: ["event-blue"] },
                 { id: 54, startDate: "2024-11-16", endDate: "2024-11-16", title: "Culto da AADECAMP", classes: ["event-write"] },
-                { id: 54, startDate: "2024-11-16", endDate: "2024-11-16", title: "Setor 9 - Missões", classes: ["event-purple"] },
-                { id: 54, startDate: "2024-11-22", endDate: "2024-11-24", title: "Setor 8", classes: ["event-purple"] },
-                { id: 54, startDate: "2024-11-22", endDate: "2024-11-23", title: "Setor 20", classes: ["event-purple"] },
+                { id: 54, startDate: "2024-11-16", endDate: "2024-11-16", title: "Setor9 - Missões", classes: ["event-purple"] },
+                { id: 54, startDate: "2024-11-22", endDate: "2024-11-24", title: "Setor8", classes: ["event-purple"] },
+                { id: 54, startDate: "2024-11-22", endDate: "2024-11-23", title: "Setor20", classes: ["event-purple"] },
                 { id: 54, startDate: "2024-11-30", endDate: "2024-11-30", title: "80 Anos", classes: ["event-yellow"] },
                 { id: 56, startDate: "2024-12-02", endDate: "2024-12-02", title: "Consagração da UFADECAMP", classes: ["event-write"] },
-                { id: 54, startDate: "2024-12-06", endDate: "2024-12-08", title: "Setor 11", classes: ["event-purple"] },
+                { id: 54, startDate: "2024-12-06", endDate: "2024-12-08", title: "Setor11", classes: ["event-purple"] },
                 { id: 59, startDate: "2024-12-07", endDate: "2024-12-07", title: "Consagração da UFADECAMP", classes: ["event-write"] },
                 { id: 60, startDate: "2024-12-07", endDate: "2024-12-07", title: "Culto da UMADECAMP", classes: ["event-write"] },
                 { id: 61, startDate: "2024-12-05", endDate: "2024-12-05", title: "Marcos Aurelio", classes: ["event-blue"] },
@@ -137,7 +137,6 @@ export default {
             if (top + tooltipHeight > viewportHeight) {
                 top = viewportHeight - tooltipHeight - 10;
             }
-
             this.tooltipContent = item.title;
             this.tooltipStyle = {
                 top: `${top}px`,
@@ -223,7 +222,7 @@ export default {
 
 @media (max-width: 600px) {
     .cv-item {
-        font-size: 12px !important;
+        font-size: 10px !important;
     }
 
     .theme-default .cv-header .periodLabel {
@@ -242,6 +241,14 @@ export default {
 
     .tooltip {
         font-size: 10px;
+    }
+
+    .previousYear, .nextYear {
+        display: none;
+    }
+
+    .cv-item.span2 {
+        max-height: 40px !important;
     }
 }
 </style>
